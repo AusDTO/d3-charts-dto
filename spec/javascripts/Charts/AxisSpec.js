a3a59dd8c1039b6cdacb272f34b17b37f8b9f7d1
chloe <chloeleichen@gmail.com>
2016-06-21 12:16:09 +1000

    fix test

--- spec/javascripts/Charts/AxisSpec.js
+++ spec/javascripts/Charts/AxisSpec.js
@@ -3,7 +3,6 @@ import Axis from '../../../lib/javascripts/Charts/Axis';
 module.exports = function(callback) {
   describe('Axis base', () => {
     let chart;
-    let axis;
 
     beforeAll(()=>{
       if (!callback) {
@@ -14,10 +13,6 @@ module.exports = function(callback) {
         chart = callback().chart;
         axis = callback().axis;
       }
-    });
-
-    it('should append group to chart svg', () => {
-      expect(chart.svg.append).toHaveBeenCalled();
     });
   });
 };
