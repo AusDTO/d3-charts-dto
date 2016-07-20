782c597e4abf264e0872456cd4acc10f3820d912
elisechant <elise.chant@gmail.com>
2016-07-20 12:51:56 +1000

    adding example stuffs for bar charts; export as es6 as well as commonjs;

--- example/src/pages/chart.js
+++ example/src/pages/chart.js
@@ -1,5 +1,10 @@
 import d3 from 'd3';
 import Chart from './../../../lib/javascripts/Charts/Chart';
+import BarChart from './../../../lib/javascripts/Charts/BarChart';
+import StackedBarChart from './../../../lib/javascripts/Charts/StackBarChart';
+
+import { DATA1, DATA4, DATA3, DATA5 } from './../fixtures/data';
+import { CON_DATA1 } from './../fixtures/formatted';
 
 
 export default class {
@@ -9,10 +14,30 @@ export default class {
 	}
 	init() {
 		this.initBasic();
+		this.initBasicBar();
+		this.initStackedBar();
+		return this;
+	}
+	initStackedBar() {
+		this.chart3 = new StackedBarChart({
+			element: d3.select('#chart-stacked-bar'),
+			data: CON_DATA1
+		});
+		this.chart3.init();
+	}
+	initBasicBar() {
+		this.chart2 = new BarChart({
+			element: d3.select('#chart-basic-bar'),
+			data: CON_DATA1
+		});
+		this.chart2.init();
 	}
 	initBasic() {
-		this.basicChart = new Chart({
-			element: d3.select('#chart-basic')
+		this.chart1 = new Chart({
+			element: d3.select('#chart-basic'),
+			data: DATA1
 		});
+		this.chart1.init();
+		this.chart1.render();
 	}
 }
